a00626941aa436d2ce5383f9e0cb2526b9171404
Isabel Espriella <isaa@MacBook-Air-de-Isabel.local>
2025-06-18 17:55:25 -0600

    Archivos por usar

---
+++ GameVault/Components/buscador.swift
@@ -0,0 +1,7 @@
+//
+//  buscador.swift
+//  GameVault
+//
+//  Created by Isabel Espriella on 18/06/25.
+//
+
---
+++ GameVault/Components/homeButton.swift
@@ -0,0 +1,7 @@
+//
+//  homeButton.swift
+//  GameVault
+//
+//  Created by Isabel Espriella on 18/06/25.
+//
+
