806b1ac09381397df11ff98b0d259fe4857c461a
Elena Isabel Espriella Bustamante <147296572+isaaaespriella@users.noreply.github.com>
2025-06-18 17:59:03 -0600

    Update homeButton.swift

--- GameVault/Components/homeButton.swift
+++ GameVault/Components/homeButton.swift
@@ -1,7 +1,40 @@
-//
-//  homeButton.swift
-//  GameVault
-//
-//  Created by Isabel Espriella on 18/06/25.
-//
+import SwiftUI
+
+struct homeButton: View {
+    var profilePic: String
+    var body: some View {
+        Button {
+            print("Presionando botón")
+        } label: {
+            AsyncImage(url: URL(string: "https://images.pexels.com/photos/697509/pexels-photo-697509.jpeg?cs=srgb&dl=pexels-andrewperformance1-697509.jpg&fm=jpg")) { phase in
+                if let image = phase.image {
+                    image
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 32, height: 32)
+                        .clipShape(Circle())
+                        .padding(1)
+                        .overlay {
+                            Circle()
+                                .stroke(Color.white.opacity(0.5), lineWidth: 1)
+                        }
+                } else if phase.error != nil {
+                    // Error al cargar la imagen
+                    Image(systemName: "xmark.octagon")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 150, height: 150)
+                        .foregroundColor(.red)
+                } else {
+                    // Cargando...
+                    ProgressView()
+                        .frame(width: 150, height: 150)
+                }
+            }
+            
+        }
+        .buttonStyle(PlainButtonStyle())
+    }
+}
+
 
